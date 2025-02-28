e040c4c1ca0214494cdf1fb8d38dca1667191435
Quentin Biache <nitrogenium@outlook.fr>
2025-02-28 20:53:02 +0100

    completed the full description and prepared the next step (trajectories)

--- src/study_2_Trajectories.m
+++ src/study_2_Trajectories.m
@@ -1,11 +1,11 @@
 % =============================================================================
 % Project       : wavePoles
-% Module name   : study_1_Chebychev
-% File name     : study_1_Chebychev.m
+% Module name   : study_2_Chebychev
+% File name     : study_2_Chebychev.m
 % File type     : Matlab script
-% Purpose       : decompose a square wave onto Chebychev polynomials
+% Purpose       : study on the poles trajectories
 % Author        : QuBi (nitrogenium@outlook.fr)
-% Creation date : Sunday, 23 February 2025
+% Creation date : Friday, 28 February 2025
 % -----------------------------------------------------------------------------
 % Best viewed with space indentation (2 spaces)
 % =============================================================================
@@ -13,39 +13,10 @@
 % -----------------------------------------------------------------------------
 % DESCRIPTION
 % -----------------------------------------------------------------------------
-% Synthesize a square wave or a sawtooth by distorting a sine wave through
-% carefully selected and weighted polynomials.
-%
-% Synthesis is done by means of Chebychev polynomials.
-%
-% In a nutshell: applying any polynomial to a sine wave will result in a wave 
-% with a bunch of harmonics (always in finite amount though)
-%
-% But when applying the Chebychev polynomials, something special happens: one
-% and only one harmonic pops up.
-% ILLUSTRATION:
-% - T2(sin(x)) = sin(2x)
-% - T3(sin(x)) = sin(3x)
-% - etc.
-% It's like a magic wand turning a sinewave of a given frequency to another 
-% sinewave with a frequency doubled, tripled, ... anything you like. Just pick
-% the right polynomial for it.
-% 
-% By weighting the Chebychev polynomials appropriately, you can synthesize any
-% periodic waveform.
-% 
-% Set a PWM ratio (between 0 and 1), hit run and observe:
-% - the synthesized waveform
-% - the roots of the sine/cosine polynomials
-% 
-% Next studies will focus on:
-% - numerical stability of the evaluation (polynomial order can get very high)
-% - curves and space spanned by the roots of the total synthesis polynomial. 
-%   Is there anything remarquable about them?
-%
-% NOTE: this version of the script does not implement edge cases of PWM = 0.0 or 
-%       1.0 so the script it might fail. 
-%       
+% Observe the trajectory of the roots as the PWM changes.
+
+% TODO: plot using a color map so that a color gradient shows as the PWM is
+%       modulated.
 
 
 close all
@@ -106,26 +77,9 @@ xSin = sin(2*pi*x);
 y = polyval(pCos, xCos) + xSin.*polyval(pSin, xCos);
 
 % -----------------------------------------------------------------------------
-% PLOTS: TIME SERIES + ZEROS REPRESENTATION
+% PLOTS: ROOT TRAJECTORIES
 % -----------------------------------------------------------------------------
-
-% Plot the time series (square wave in time)
-figure
-plot(x,y)
-xlim([-0.75, 0.75])
-grid minor
-
-% Alternate representation of the polynomials: zeros plot
-figure
-rCos = roots(pCos);
-rSin = roots(pSin);
-plot(real(rCos(2:end)), imag(rCos(2:end)), '+', real(rSin(2:end)), imag(rSin(2:end)), '+')
-legend('cos', 'sin')
-xlim([-1.3, 1.3])
-ylim([-0.5, 0.5])
-grid on
-title(sprintf('r = %0.3f', r))
-axis equal;
+% TODO
 
 
 
